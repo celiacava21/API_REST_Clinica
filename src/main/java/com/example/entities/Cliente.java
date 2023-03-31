15e16a56a3cb8e08071e6bdc8ae11ecbdbef0bda
celia <celia.cava18ruiz@gmail.com>
2023-03-31 18:42:44 +0200

    entidades terminadas

---
+++ src/main/java/com/example/entities/Cliente.java
@@ -0,0 +1,43 @@
+package com.example.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+    private String apellidos;
+
+   
+    private LocalDate fechaAlta;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private Hotel hotel;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST, mappedBy = "cliente")
+    private List<Mascota> mascotas;
+}
